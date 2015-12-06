2479cbaef6d36091f8b0b3432da2c1c4caa30a5a
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2015-12-06 02:07:29 +0100

    Add dependencies

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@
 from setuptools import setup
 
 setup(name='sympy_recursive',
-      version='0.1',
+      version='0.1.1',
       description='Resolve recursive sequences to explicit nth item formulas with SymPy.',
       url='http://github.com/hejmsdz/sympy_recursive',
       author='Mikołaj Rozwadowski',
       author_email='mikolaj.rozwadowski@outlook.com',
       license='MIT',
       packages=['sympy_recursive'],
+      install_requires=[
+          'sympy',
+      ],
       zip_safe=False)
